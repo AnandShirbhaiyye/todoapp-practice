28bdc1fb6d30214c63d16ad5628aa7db6dd4c4af
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-06-14 00:14:32 +0530

    added mongo db url

--- server/index.js
+++ server/index.js
@@ -3,11 +3,25 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
+mongoose.set('strictQuery', false);
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+try {
+  mongoose.connect(
+    process.env.MONGODB_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+      console.log('Connected to DB 📦');
+    }
+  );
+} catch (err) {
+  console.log(`❌ Error:  ${err?.message}`);
+}
+
 
 
 app.listen(PORT, () => {
