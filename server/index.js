93cf7d94922635762e9965886389a15b15cc077b
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-06-14 00:09:15 +0530

    create basic set up of server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+    console.log(`The server is Running on Port ${PORT} 🚀`);
+  });
