5ae541f553ad5ad23c93c3219bf78a290b3e476d
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-06-16 23:02:13 +0530

    added new monga url

--- server/index.js
+++ server/index.js
@@ -1,26 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
 dotenv.config();
-mongoose.set('strictQuery', false);
 
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-
-try {
-  mongoose.connect(
-    process.env.MONGODB_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-      console.log('Connected to DB 📦');
-    }
-  );
-} catch (err) {
-  console.log(`❌ Error:  ${err?.message}`);
+async function connectMongoDB(){
+  const conn = await mongoose.connect(process.env.MONGODB_URL);
+  if(conn)
+  {
+    console.log("Connected to MongoDB");
+  }
 }
+connectMongoDB();
+
+
+const PORT = process.env.PORT || 5000;
 
 app.get('/health', (req, res)=>{
   res.json({
@@ -28,8 +24,6 @@ app.get('/health', (req, res)=>{
     message:"All Good🥳🥳"
   })
 })
-
-
 
 app.listen(PORT, () => {
     console.log(`The server is Running on Port ${PORT} 🚀`);
