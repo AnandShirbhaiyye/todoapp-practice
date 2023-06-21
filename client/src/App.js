117333c5044d51c3c709fe86b97c229ee788b90d
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-06-21 17:03:45 +0530

    added basic design of todoapp task

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,50 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-   <>
-   <div className='text-center'>
-       <h1>Hello Anand...</h1>
-   </div>
-   </>
+    <>
+     <div className="container">
+        <div className="card shadow-lg mt-5 p-5">
+          <div className="text-center mb-4">
+            <h2>
+              ToDoApp
+            </h2>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card shadow-sm p-3">
+                <form>
+                  <h4 className="text-center mt-3 mb-4">➕Add Tasks</h4>
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter Title"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter Discription"
+                    />
+                  </div>
+                  <button
+                    className="login-page-btn btn btn-primary w-100 mb-3"
+                    type="button"
+                  >
+                    <i class="fa-solid fa-right-to-bracket"></i> Send
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className="col-md-6">
+             Hello
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
