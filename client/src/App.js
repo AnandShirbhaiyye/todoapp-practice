9c5c34faf145ade462c25eefd526783e02adf716
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-06-21 17:16:14 +0530

    create  post api

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  const[title, setTitle] = useState("");
-  const [description, setDescription] = useState("")
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const addTask = async () => {
+    const { data } = await axios.post("/task", {
+      title: title,
+      description: description,
+    });
+    alert(data?.message);
+    setTitle("");
+    setDescription("");
+  };
   return (
     <>
-     <div className="container">
+      <div className="container">
         <div className="card shadow-lg mt-5 p-5">
           <div className="text-center mb-4">
-            <h2>
-              ToDoApp
-            </h2>
+            <h2>ToDoApp</h2>
           </div>
           <div className="row">
             <div className="col-md-6">
@@ -24,6 +33,9 @@ function App() {
                       className="form-control"
                       placeholder="Enter Title"
                       value={title}
+                      onChange={(e) => {
+                        setTitle(e.target.value);
+                      }}
                     />
                   </div>
                   <div className="mb-3">
@@ -32,20 +44,23 @@ function App() {
                       className="form-control"
                       placeholder="Enter Discription"
                       value={description}
+                      onChange={(e) => {
+                        setDescription(e.target.value);
+                      }}
                     />
                   </div>
                   <button
                     className="login-page-btn btn btn-primary w-100 mb-3"
                     type="button"
+                    onClick={addTask}
                   >
-                    <i class="fa-solid fa-right-to-bracket"></i> Send
+                    {" "}
+                    addTask
                   </button>
                 </form>
               </div>
             </div>
-            <div className="col-md-6">
-             Hello
-            </div>
+            <div className="col-md-6">Hello</div>
           </div>
         </div>
       </div>
